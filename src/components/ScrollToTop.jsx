96c5f280a0ca1d3c28afbce66579fbebcb357ce2
AhmedmMhmoud2001 <ahmedmahmoud2152001@gmail.com>
2026-01-27 16:20:57 +0200

    update

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -5,12 +5,8 @@ const ScrollToTop = () => {
     const { pathname, hash } = useLocation();
 
     useEffect(() => {
-        // If there is a hash, let the Home.jsx or another component handle the smooth scroll
-        // Otherwise, scroll to the top of the page
-        if (!hash) {
-            window.scrollTo(0, 0);
-        }
-    }, [pathname, hash]);
+        window.scrollTo(0, 0);
+    }, [pathname]);
 
     return null;
 };
